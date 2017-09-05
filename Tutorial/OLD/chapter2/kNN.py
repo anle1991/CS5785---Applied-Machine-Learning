5c0e95d07878e96b7b96fce0aa7a7c20e6a67a33
An Le <an.le@sv.cmu.edu>
2017-09-04 23:58:20 -0400

    C2

--- Tutorial/OLD/chapter2/kNN.py
+++ Tutorial/OLD/chapter2/kNN.py
@@ -23,3 +23,18 @@ def classify0(inX, dataSet, labels, k):
                               key=operator.itemgetter(1), 
                               reverse=True)
     return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
